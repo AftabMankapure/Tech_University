9bc40eeebb7afe61141f165dceee46e54991b28c
aftabmankapure <aftabmankapure@gmail.com>
2023-07-24 18:28:26 +0530

    added about and new_courses page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'courses#index'
+  get 'about', to: 'pages#about'
+  get '/courses/new', to: 'courses#new'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
