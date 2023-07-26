eb0b1de11333acd128bec54319e80ecce763575f
aftabmankapure <aftabmankapure@gmail.com>
2023-07-26 12:31:28 +0530

    display student at student view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'about', to: 'pages#about'
   get '/courses/new', to: 'courses#new'
-
+  resources :students  ,except: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
